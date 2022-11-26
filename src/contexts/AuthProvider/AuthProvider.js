9a7e8d645dda73717f6273d31ac9cb0f7749a9ad
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-11-26 18:01:10 +0600

    add firebase and env

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,22 @@
+import React, { createContext } from 'react';
+import app from '../../firebase/firebase.config';
+import {getAuth} from 'firebase/auth';
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+
+    const authInfo = {
+
+    }
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
