bd6ec3e90e8fdd07a47c926fe939f5396c021595
mdmrmmunna3 <mdmunnaking1209@gmail.com>
2022-11-26 14:22:46 +0600

    create navbar

---
+++ src/Layouts/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Nabvar from '../Pages/Shared/Navbar/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            <Nabvar></Nabvar>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,15 @@
+import Main from "../../Layouts/Main";
+
+const { createBrowserRouter } = require("react-router-dom");
+
+const router = createBrowserRouter([
+    {
+        path: '/', 
+        element: <Main></Main>,
+        children: [
+            
+        ]
+    }
+])
+
+export default router ;
